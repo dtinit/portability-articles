8586ff028fe0f11eff6feab9f30f557dfca4703d
Aarón Ayerdis Espinoza <aaronaej7@gmail.com>
2024-03-21 23:01:28 -0600

    Update sanity check

--- sanity_check.sh
+++ sanity_check.sh
@@ -5,7 +5,7 @@ echo "Loading the Markdown files that have changed"
 for x in $(git diff --name-only HEAD origin/main -- "articles/*.md")
 do
   is_valid_md=$(python validate_markdown_metadata.py $x)
-  if $is_valid_md; then
+  if [[ $is_valid_md == "True"]]; then
     echo "Validating file $x"
   else
     echo "File $x failed"
